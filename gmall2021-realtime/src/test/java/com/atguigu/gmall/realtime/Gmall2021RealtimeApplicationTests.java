d8ff313c65a4ce0f8ce96a1913f03277f6cfda14
szp <sunzhipeng96@163.com>
2021-04-27 21:45:10 +0800

    Flink大数据实时数仓

---
+++ gmall2021-realtime/src/test/java/com/atguigu/gmall/realtime/Gmall2021RealtimeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atguigu.gmall.realtime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Gmall2021RealtimeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
